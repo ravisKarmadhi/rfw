726de90ab22487c0ee497a240a6ac335503b0160
jigishachavda <jigishachavda1011@gmail.com>
2024-05-10 11:11:52 +0530

    event-open design

---
+++ templates/event-open.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * Template Name: Event Open
+ */
+?>
+<main>
+    <div class="spacing dpt-230"></div>
+    <section class="event-open-section">
+        <div class="container">
+            <a href="" class="text-decoration-none back-all d-flex align-item-center dmb-50 classic font-18 leading-20 fw-normal text-2C2924">
+                <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/05/arrow-prev.svg" alt="arrow-prev.svg" class="me-2">
+                Back To All
+            </a>
+            <div class="classic font-38 leading-40 fw-normal text-2C2924 dmb-30">
+                Wine Tasting
+            </div>
+            <div class="classic font-18 leading-20 fw-normal text-2C2924 dmb-50">
+                April 4 @ 9.30am
+            </div>
+            <div class="event-open-img dmb-65">
+                <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/05/video-3.png" alt="video-3.png" class="h-100 w-100 object-cover">
+            </div>
+            <div class="col-11 pe-4 dmb-75">
+                <div class="classic font-16 leading-28 fw-normal text-2C2924 dmb-30">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </div>
+                <div class="classic font-16 leading-28 fw-normal text-2C2924 dmb-30">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem i
+                </div>
+            </div>
+            <div class="col-3 dmb-65">
+                <div class="classic font-16 leading-28 fw-normal text-2C2924 dmb-30 opacity-55 dmb-25">Details</div>
+                <div class="event-open-details">
+                    <div class="d-flex align-item-center dmb-30">
+                        <div class="classic font-20 leading-24 fw-normal text-2C2924 col-4">Date:</div>
+                        <div class="classic-med font-20 leading-24 text-2C2924 col-8">April 9</div>
+                    </div>
+                    <div class="d-flex align-item-center dmb-30">
+                        <div class="classic font-20 leading-24 fw-normal text-2C2924 col-4">Starts:</div>
+                        <div class="classic-med font-20 leading-24 text-2C2924 col-8">9.30 am</div>
+                    </div>
+                    <div class="d-flex align-item-center dmb-30">
+                        <div class="classic font-20 leading-24 fw-normal text-2C2924 col-4">Cost:</div>
+                        <div class="classic-med font-20 leading-24 text-2C2924 col-8">Free</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 pe-2">
+                <div class="book-ticket-box border-EBEBEB dpt-35 dpb-40 radius-5">
+                    <div class="classic font-16 leading-20 fw-normal text-2C2924 dpb-25">Book tickets</div>
+                    <div class="d-flex align-item-center book-ticket-item border-top-EBEBEB border-bottom-EBEBEB dpt-30 dpb-30 dmb-25">
+                        <div class="col-8 classic font-22 leading-24 fw-normal text-2C2924 text-capitalize pe-3">Wine Tasting</div>
+                        <div class="col-1 classic font-22 leading-24 fw-normal text-2C2924 text-capitalize text-center">Free</div>
+                        <div class="col-3 classic-med font-16 leading-24 text-2C2924 text-capitalize text-end">2 available</div>
+                    </div>
+                    <button class="btnA bgdark-BDA673-btn">Get Tickets</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="spacing dmb-185"></div>
+</main>
