d76fc1a399b873b0aa55acf5c30ffcb097ec38a7
truptikarmadhi <132331501+truptikarmadhi@users.noreply.github.com>
2024-05-13 16:44:08 +0530

    contact complete

---
+++ templates/contact.php
@@ -0,0 +1,148 @@
+<?php
+/*
+ * Template Name: Contact
+ */
+?>
+<main class="bg-2C2924">
+    <!-- contact-section -->
+    <section class="contact-section contact-bg">
+        <div class="container">
+            <div class="row ">
+                <div class="col-12 col-xl-8 mx-auto px-lg-4 mpt-235 tpt-185 dpt-135">
+                    <div
+                        class="col-lg-9 mx-auto px-4 px-lg-3 d-flex flex-wrap justify-content-between dmb-55 tmb-0 tpb-30">
+                        <div class="col-12 col-md-5 tmb-65">
+                            <div class="font-50 leading-56 louise text-center text-FFFAF6 dmb-25">Contact Details </div>
+                            <div class="d-flex align-items-center justify-content-center dmb-10">
+                                <div class="me-2 contact-icon">
+                                    <img src="<?php echo get_home_url(); ?> /wp-content/uploads/2024/05/contact-call.svg"
+                                        alt="">
+                                </div>
+                                <a href="tel:01283-330-060"
+                                    class="font-15 leading-20 classic fw-normal text-center text-FFFAF6 text-decoration-none">01283
+                                    330 060
+                                </a>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-center dmb-10">
+                                <div class="me-2 contact-icon">
+                                    <img src="<?php echo get_home_url(); ?> /wp-content/uploads/2024/05/contact-mail.svg"
+                                        alt="">
+                                </div>
+                                <a href="mailto:info@rfweddings.co.uk"
+                                    class="font-15 leading-20 classic fw-normal text-center text-FFFAF6 text-decoration-none">info@rfweddings.co.uk
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-5 tmb-65">
+                            <div class="font-50 leading-56 louise text-center text-FFFAF6 dmb-25">Find Us </div>
+                            <div class="d-inline-flex ">
+                                <div class="me-2 contact-icon">
+                                    <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/05/contact-location.svg"
+                                        alt="">
+                                </div>
+                                <a href="https://www.google.com/maps/place/Royle+Forest+Wedding+Venue/@52.7669309,-1.6244195,17z/data=!3m1!4b1!4m6!3m5!1s0x487a0173d7c9372d:0xaf3c6fcf06537052!8m2!3d52.7669309!4d-1.6218446!16s%2Fg%2F11jn1p571x?entry=ttu"
+                                    class="font-15 leading-20 classic fw-normal text-center text-FFFAF6 text-decoration-none">Royle Farm
+                                    Forest,
+                                    Caldwell Road Drakelow, Burton Upon Trent Staffordshire, DE15 9TU
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="font-25 lh-1 classic fw-normal text-center text-FFFAF6 tmb-30 dmb-20">Get in touch
+                            via our
+                            form</div>
+                        <form>
+                            <div class="d-flex flex-wrap">
+                                <div class="col-12 col-lg-6 form-input px-lg-2 tmb-10 dmb-15">
+                                    <input type="text" placeholder="Name…"
+                                        class="font-15 leading-16 classic fw-normal h-100 w-100 bg-36332D border-0 px-4 text-FFFAF6 radius-3">
+                                </div>
+                                <div class="col-12 col-lg-6 form-input px-lg-2 tmb-10 dmb-15">
+                                    <input type="text" placeholder="Email..."
+                                        class="font-15 leading-16 classic fw-normal h-100 w-100 bg-36332D border-0 px-4 text-FFFAF6 radius-3">
+                                </div>
+                                <div class="col-12 col-lg-6 form-input px-lg-2 tmb-10 dmb-15">
+                                    <input type="text" placeholder="Number…"
+                                        class="font-15 leading-16 classic fw-normal h-100 w-100 bg-36332D border-0 px-4 text-FFFAF6 radius-3">
+                                </div>
+                                <div class="col-12 col-lg-6 form-input px-lg-2 tmb-10 dmb-15">
+                                    <input type="text" placeholder="Subject…"
+                                        class="font-15 leading-16 classic fw-normal h-100 w-100 bg-36332D border-0 px-4 text-FFFAF6 radius-3">
+                                </div>
+                                <div class="col-12 px-lg-2 tmb-10 dmb-15 form-textarea">
+                                    <textarea type="text" placeholder="Message…"
+                                        class="font-15 leading-16 classic fw-normal h-100 w-100 bg-36332D border-0 px-4 text-FFFAF6 pt-3 radius-3">
+                                        </textarea>
+                                </div>
+                                <div
+                                    class="col-12 custom-checkbox pt-lg-2 tmt-25 tmb-30 dmb-25 d-flex align-items-center justify-content-center">
+                                    <input type="checkbox" id="effect_one" class="checkbox" name="blur">
+                                    <label for="effect_one" class="font-15 lh-1 classic text-FFFAF6 ">Check to sign up
+                                        to our newsletter
+                                    </label>
+                                </div>
+                                <a
+                                    class="btnA bgdark-BDA673-btn classic font-14 leading-16 text-capitalize text-decoration-none mx-auto">
+                                    Submit Form
+                                </a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <!-- three-card-section -->
+    <div class="spacing tmb-70 dmb-155"></div>
+    <section class="three-card-section">
+        <div class="container px-p-0">
+            <div class="row row-8">
+                <div class="col-12 col-lg-4 px-2 three-cards mb-2">
+                    <a href="javascript:void(0)"
+                        class="three-card h-100 w-100 card-hover position-relative overflow-hidden d-inline-block">
+                        <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/05/gallary-1.png" alt=""
+                            class="w-100 h-100 object-cover img">
+                        <div class="card-layer position-absolute w-100 h-100 bg-black top-0"></div>
+                        <div class="position-absolute top-left-center">
+                            <div
+                                class="btnA bgdark-BDA673-btn res-bg-2C2924-btn classic font-14 leading-16 text-capitalize text-decoration-none mx-auto">
+                                FAQS
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-12 col-lg-4 px-2 three-cards mb-2">
+                    <a href="javascript:void(0)"
+                        class="three-card h-100 w-100 card-hover position-relative overflow-hidden d-inline-block">
+                        <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/05/blog-1.png" alt=""
+                            class="w-100 h-100 object-cover img">
+                        <div class="card-layer position-absolute w-100 h-100 bg-black top-0"></div>
+                        <div class="position-absolute top-left-center">
+                            <div
+                                class="btnA bgdark-BDA673-btn res-bg-2C2924-btn classic font-14 leading-16 text-capitalize text-decoration-none mx-auto">
+                                Book A Visit
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-12 col-lg-4 px-2 three-cards mb-2">
+                    <a href="javascript:void(0)"
+                        class="three-card h-100 w-100 card-hover position-relative overflow-hidden d-inline-block">
+                        <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/05/pagination-7.png" alt=""
+                            class="w-100 h-100 object-cover img">
+                        <div class="card-layer position-absolute w-100 h-100 bg-black top-0"></div>
+                        <div class="position-absolute top-left-center">
+                            <div
+                                class="btnA bgdark-BDA673-btn res-bg-2C2924-btn classic font-14 leading-16 text-capitalize text-decoration-none mx-auto">
+                                Submit Form
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="spacing tmb-85 dmb-180"></div>
+</main>
